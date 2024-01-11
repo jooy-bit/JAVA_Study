6f6dbea1ace0e3106dc99800696b2484256a24e8
jooy-bit <hjooy6099@gmail.com>
2024-01-11 13:46:04 +0900

    set 자료구조

---
+++ JAVA/partC/src/collection/C03MemberListPrint.java
@@ -0,0 +1,40 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import collection.day10.Member;
+
+public class C03MemberListPrint {
+    public static void main(String[] args) {
+        List<Member> list = new ArrayList<>();
+        System.out.println("1234567890123456789012345678901234567890");
+        list.add(new Member("momo",23));
+        list.add(new Member("nana",22));
+        list.add(new Member("momo",25));
+        list.add(new Member("dahy",21));
+        list.add(new Member("sana",26));
+        //toString 재정의된 출력이 아닌 직접 이름, 나이를 모두 출력
+        System.out.println(String.format("%15s\t %15s", "name","age"));
+        for (int i = 0; i < list.size(); i++) System.out.println(String.format("%15s\t %15d", list.get(i).getName(),list.get(i).getAge()));
+    
+    //컬렉션에서는 반복자(Iterator)로 데이터에 접급
+    //반복자를 이용한 출
+    Iterator<Member> iterator = list.iterator(); //list로 선언된 자료구조에 접근할 반복자 선언
+    int count =0;
+    while(iterator.hasNext()){          //반복자가 접근할 다음 데이터가 있으면 참
+        Member temp = iterator.next();  //반복자가 다음 데이터를 가져옵니다
+        System.out.println("\tcount="+count++ +", 요소="+temp);
+    }
+    
+    //iterator 로 동작하는 for
+    count =0;
+    for (Member temp : list) System.out.println("\tcount="+count++ +", 요소="+temp);
+    
+
+
+
+    
+    }//main
+}//class
---
+++ JAVA/partC/src/collection/day10/C04SetExample.java
@@ -0,0 +1,60 @@
+package collection.day10;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class C04SetExample {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("트와이스");
+        set.add("아이브");
+        set.add("잇지");
+        set.add("뉴진스");
+        set.add("블랙핑크");
+        set.add("트와이스");
+        System.out.println("~~~~~HashSet~~~~~");
+        System.out.println("set: "+set);
+        System.out.println("set.size(): "+set.size());
+        /* 
+         * ~~~~~HashSet~~~~~
+            set: [아이브, 잇지, 트와이스, 뉴진스, 블랙핑크]
+            set.size(): 5
+
+
+         */
+
+        Set<String> set2 = new LinkedHashSet<>();   
+        //본래 Set은 무작위 접근 
+        //Linked는 다음 입력데이터의 참조값을 저장하여 입력순서대로 접근
+        set2.add("트와이스");
+        set2.add("아이브");
+        set2.add("잇지");
+        set2.add("뉴진스");
+        set2.add("소녀시대");
+        set2.add("블랙핑크");
+        set2.add("트와이스");
+        System.out.println("~~~~~LinkedHashSet~~~~~");
+        System.out.println("set2: "+set2);
+        System.out.println("set2.size(): "+set2.size());
+        
+        Set<String> set3 = new TreeSet<>();
+        set3.add("트와이스");
+        set3.add("아이브");
+        set3.add("잇지");
+        set3.add("뉴진스");
+        set3.add("소녀시대");
+        set3.add("블랙핑크");
+        set3.add("트와이스");
+        System.out.println("~~~~~TreeSet~~~~~");
+        System.out.println("set3: "+set3);
+        System.out.println("set3.size(): "+set3.size());
+    }
+}
+/* 
+ * Set: 수학시간에 배우 집합
+ * 순서(인덱스) X
+ * 중복값 X
+ * Map 구성요소 : key, Value 중에서 Key는 Set 특성을 지님
+ */
