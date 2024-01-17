50bb918f4615019c17e34b3f86993f8d0e0f4121
jooy-bit <hjooy6099@gmail.com>
2024-01-17 13:06:33 +0900

    review

--- JAVA/partC/src/io/day13/ReviewProb.java
+++ JAVA/partC/src/io/day13/ReviewProb.java
@@ -8,7 +8,7 @@ import io.javaword.JavaWordList;
 
 public class ReviewProb {
     public static void main(String[] args) throws IOException {
-        prod_8();
+        prod_10();
     }
     public static void prod_8(){
         JavaWordList words =new JavaWordList("단어장.txt");
@@ -20,10 +20,31 @@ public class ReviewProb {
         //2) 기존 발생하는 예외를 새로운 내용으로 변경할 때
     }
 
-    public static void prod_10()throws IOException{
+    /* public static void prod_10() throws IOException {
         byte[] input = new byte[20];
         System.in.read(input);
         System.out.println(new String(input));
+    } */
+    public static void prod_10(){
+        byte[] buffer =new byte[20];
+        try {
+            int b = System.in.read(buffer);
+            System.out.println("입력한 바이트 수:  "+b);
+            //System.out.println("입력한 문자열:  "+new String(buffer));
+            System.out.println("입력한 문자열 정수 변환:  "+ Integer.parseInt(new String(buffer).trim()));
+        } catch (IOException | NumberFormatException e) {
+            //2개의 예외를 처리합니다 참고 : || 논리적인 OR, | 비트단위 OR
+            System.out.println("IOException 또는 NumberFormatException 예외를 처리합니다");
+            e.printStackTrace();
+            /* 
+             *  Exception in thread "main" java.lang.NumberFormatException: For input string: "abc"
+                at java.base/java.lang.NumberFormatException.forInputString(NumberFormatException.java:67)
+                at java.base/java.lang.Integer.parseInt(Integer.java:668)
+                at java.base/java.lang.Integer.parseInt(Integer.java:786)
+                at io.day13.ReviewProb.prod_10(ReviewProb.java:34)
+                at io.day13.ReviewProb.main(ReviewProb.java:11)
+             */
+        }
     }
     
 }
