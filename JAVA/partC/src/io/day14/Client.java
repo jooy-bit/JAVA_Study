a1722d6864445956772ffdb8c10f06452b534934
jooy-bit <hjooy6099@gmail.com>
2024-01-17 14:48:11 +0900

    서버열고 파일,문자열 주고 받기

--- JAVA/partC/src/io/day14/Client.java
+++ JAVA/partC/src/io/day14/Client.java
@@ -1,5 +1,50 @@
 package io.day14;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+//socket.png 그림참고 : 클라이언트와 서버 각각 실행하는 메소드가 다릅니다.
+                    //  서버와 클라이먼트 연결 과정이 다르기 때문
 public class Client {
-    
+    public static void main(String[] args) {
+        Socket socket =null;
+        try {
+            System.out.println("[클라이언트] 프로그램 입니다");
+            socket = new Socket();
+            Thread.sleep(3000);  //시간 중지 타입 설정
+            socket.connect(new InetSocketAddress("192.168.30.11",5050)); //서버 설정한 bind 정보와 같은 객체를 만듭니다.
+            System.out.println("__연결성공 하였습니다");
+            //서버가 클라이언트에게 데이터 보내기 : 출력 스트림
+            //클라이언트와 서버가 연결된 소켓으로 출력 스트림 생성
+            InputStream is = socket.getInputStream();
+            DataInputStream dis = new DataInputStream(is);
+            System.out.println(dis.readUTF());
+
+            //서버에 인사말 보내기
+            OutputStream os = socket.getOutputStream();
+            DataOutputStream dos = new DataOutputStream(os);
+            System.out.print("서버에게 보낼 메시지를 쓰세요__");
+            dos.writeUTF("\tForm 클라이언트 >>"+ System.console().readLine());
+            //이미지 파일을 서버에 보내기(업로드)
+            String filePath = "tom.gif";
+            FileInputStream fis = new FileInputStream(filePath);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            int b;
+            while ((b=bis.read())!=-1) {
+                dos.write(b);
+            }
+            System.out.println("파일 업로드 완료");
+        } catch (IOException |InterruptedException e) {
+            e.printStackTrace();
+        }finally{
+            try {socket.close();} catch (Exception e){}
+        }
+    }
 }
--- JAVA/partC/src/io/day14/Server.java
+++ JAVA/partC/src/io/day14/Server.java
@@ -1,5 +1,52 @@
 package io.day14;
 
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+//socket.png 그림참고 : 클라이언트와 서버 각각 실행하는 메소드가 다릅니다.
+                    //  서버와 클라이먼트 연결 과정이 다르기 때문
+//프로그램 실행은 서버가 먼저 실행
 public class Server {
-    
+    public static void main(String[] args) {
+        ServerSocket server = null;
+        Socket socket = null;
+        try {//서버소켓 생성
+            server = new ServerSocket(); //서버 설정을 위한 소켓 
+            server.bind(new InetSocketAddress("192.168.30.11",5050)); //서버 연결 정보 IP와 PORT 설정
+            System.out.println("[서버프로그램] : 연결 요청을 기다리는 중입니다");
+            socket = server.accept();    //입출력스트림을 위한 소켓
+            //클라이언트 연결 요청(connect)에 대한 수락
+            System.out.println("__연결을 수락합니다");         //accept 정상 완료
+            OutputStream os = socket.getOutputStream();
+            //정수,실수,문자열 보내는 스트림
+            DataOutputStream dos = new DataOutputStream(os);    //보조 스트림
+            dos.writeUTF(("\tForm 서버 >> 환영합니다"));
+            //클라이언트가 보낸 인사말 받기
+            InputStream is = socket.getInputStream();
+            DataInputStream dis = new DataInputStream(is);
+            System.out.println(dis.readUTF());
+
+            //이미지파일 받기
+            BufferedOutputStream bos =new BufferedOutputStream(new FileOutputStream("D:\\tomy.gif"));
+            int b; int count =0;
+            while ((b=dis.read())!=-1) {
+                bos.write(b);
+                count++;
+            }
+            System.out.println("파일을 받았습니다. 총 바이트 :  "+count);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try { server.close(); socket.close(); } catch (IOException e) {}
+        }
+    }
 }
