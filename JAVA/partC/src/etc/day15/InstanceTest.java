12bbb6f27e871a4df8e19a53c10d23c5dec101ec
jooy-bit <hjooy6099@gmail.com>
2024-01-18 14:38:00 +0900

    data type &oracle db download

--- JAVA/partC/src/etc/day15/InstanceTest.java
+++ JAVA/partC/src/etc/day15/InstanceTest.java
@@ -16,52 +16,69 @@ public class InstanceTest {
         MyCalendar my1 = MyCalendar.getInstance();
         // MyCalendar my2 = new MyCalendar();
         // MyCalendar my3 = MyCalendar.object;
-        YourCalendar you1 = YourCalendar.of("2024", "03");
+        YourCalendar you1 = YourCalendar.of("2024","03");
+
+        Member member = new Member("kkk", 23, 1110);
+        System.out.println(member.getAge());
     }
     
 }
-//main메소드는 1개파일에 하나만 있어야 한다 //public 클래스도 1개 파일에 하나만 있어야 한다
-// 위의 2가지 지키고 1개 파일 클래스는 여러개 만들 수 있다.
+
+//main 메소드는 1개 파일에 하나만 있어야 합니다.
+//public 클래스도 1개 파일에 하나만 있어야 합니다.
+// ㄴ 위의 2가지 지키고 1개 파일에 클래스는 여러개 만들 수 있습니다.
 
 @Getter
 @Setter
-@NoArgsConstructor
+@NoArgsConstructor          //기본생성자
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode
-class Member {
+
+class Member{
     private String name;
     private int age;
     private int point;
-    //커스텀 생성자 //기본생성자 //getter //setter //toString 재정의 //hashcode 와 equals 재정의
+
+    //커스텀 생성자
+    //기본 생성자
+    //getter //setter
+    //tpString
+    //hashcode 와 equals 재정의
 }
 
 class MyCalendar {
     private static MyCalendar object = new MyCalendar();
+    //필드가 현재 클래스의 타입. 미리 생성자로 객체를 만듭니다.
 
-    private MyCalendar() {
+    private MyCalendar() {      //생성자는 private. 다른 클래스에서 사용 못함.
     }
 
     public static MyCalendar getInstance(){
-        return object;  //미리 만들어진 클래스 타입의 객체를 리턴
+        //미리 만들어진 현재 클래스 타입의 객체를 리턴
+        return object;
     }
+
 }
 
-class YourCalendar{
+class YourCalendar {
     private String month;
     private String day;
-    private YourCalendar(){}
-    public static YourCalendar of(String month,String day){
+
+    private YourCalendar(){}         //다른 클래스에서 new연산 사용 못함.
+
+    public static YourCalendar of(String month, String day){
         YourCalendar result = new YourCalendar();
         result.month = month;
         result.day = day;
         return result;
     }
-    public String getDay(){
+
+    public String getDay() {
         return day;
     }
+
     public String getMonth() {
         return month;
     }
-
 }
---
+++ JAVA/partC/src/etc/day15/InstanceTest2.java
@@ -0,0 +1,73 @@
+package etc.day15;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * InnerMyCalendar
+ */
+public class InstanceTest2 {
+
+    public static void main(String[] args) {
+        MyCalendar my1 = MyCalendar.getInstance();
+        // MyCalendar my2 = new MyCalendar();
+        // MyCalendar my3 = MyCalendar.object;
+        YourCalendar you1 = YourCalendar.of("2024", "03");
+        
+        Member member = new Member("neae",23,1110);
+        System.out.println(member.getAge());
+    
+    }
+
+    
+}
+//main메소드는 1개파일에 하나만 있어야 한다 //public 클래스도 1개 파일에 하나만 있어야 한다
+// 위의 2가지 지키고 1개 파일 클래스는 여러개 만들 수 있다.
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+class Member {
+    private String name;
+    private int age;
+    private int point;
+    //lombok 라이브러리 : 매번 작성되는 아래 메소드를 자동으로 만들어 줍니다
+    //커스텀 생성자 //기본생성자 //getter //setter //toString 재정의 //hashcode 와 equals 재정의
+}
+
+class MyCalendar {
+    private static MyCalendar object = new MyCalendar();
+
+    private MyCalendar() {
+    }
+
+    public static MyCalendar getInstance(){
+        return object;  //미리 만들어진 클래스 타입의 객체를 리턴
+    }
+}
+
+class YourCalendar{
+    private String month;
+    private String day;
+    private YourCalendar(){}
+    public static YourCalendar of(String month,String day){
+        YourCalendar result = new YourCalendar();
+        result.month = month;
+        result.day = day;
+        return result;
+    }
+    public String getDay(){
+        return day;
+    }
+    public String getMonth() {
+        return month;
+    }
+
+}
